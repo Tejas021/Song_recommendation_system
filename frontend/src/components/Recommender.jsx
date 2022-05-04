f8020d12904de2e26836f3e06cb5ff2dda1e1169
unknown <akshayhegde158@gmail.com>
2022-05-04 16:08:37 +0530

    loader

--- frontend/src/components/Recommender.jsx
+++ frontend/src/components/Recommender.jsx
@@ -1,31 +1,41 @@
 import React, { useState } from 'react'
 import { request } from '../axios'
 import "../styles/recommender.scss"
+import loading from './loader.webp'
+
+
 const Recommender = () => {
-    const [info, setInfo] = useState({name:"",songname:""})
-    const [songs, setSongs] = useState(null)
-    const submitReq=async(e)=>{
-        e.preventDefault()
-        request.post("/song",info).then(res=>res.data).then(res=>setSongs(res))
-        
-    }
+  const [info, setInfo] = useState({ name: "", songname: "" })
+  const [songs, setSongs] = useState(null)
+  const [loader,setLoader]=useState(false)
+  const submitReq = async (e) => {
+    e.preventDefault()
+    setLoader(true)
+    request.post("/song", info).then(res => res.data).then(res => {setSongs(res); setLoader(false)})
+
+  }
   return (
     <div className='recommender_container'>
-        <h2 className='title'>Find Recommendation</h2>
-        <form onSubmit={e=>submitReq(e)}>
-         
-            <input type="text" placeholder="songname" onChange={(e)=>setInfo({...info,songname:e.target.value})}/>
-            <input type="text" placeholder="artist" onChange={(e)=>setInfo({...info,name:e.target.value})}/>
-            <button type="submit " >Submit</button>
-        </form>
+      <h2 className='title'>Find Recommendation</h2>
+      <form onSubmit={e => submitReq(e)}>
 
-<div className='row  '> 
-        {songs?songs.map(sg=><div className=' col-md-3'><div class="card bg-success my-3 " style={{width: "100%"}}>
-  <img src="https://images.squarespace-cdn.com/content/v1/5bef06e6fcf7fd06b84df720/1546871259656-4VRXA8OLY0XHMVEKMDOW/Devon%2BSong%2BBanner.jpg?format=2500w" class="card-img-top" alt="..."/>
-  <div class="card-body ">
-    <p class="card-text "><b>{sg.song}</b></p>
-  </div></div>
-</div>):<div>wait</div>}</div>
+        <input type="text" placeholder="songname" onChange={(e) => setInfo({ ...info, songname: e.target.value })} />
+        <input className='mt-2' type="text" placeholder="artist" onChange={(e) => setInfo({ ...info, name: e.target.value })} />
+        <button className='mt-4' type="submit " >Submit</button>
+      </form>
+
+      <div className='row  '>
+        {songs ? songs.map(sg => <div className=' col-md-3'><div class="card bg-success my-3 " style={{ width: "100%" }}>
+          <img src="https://images.squarespace-cdn.com/content/v1/5bef06e6fcf7fd06b84df720/1546871259656-4VRXA8OLY0XHMVEKMDOW/Devon%2BSong%2BBanner.jpg?format=2500w" class="card-img-top" alt="..." />
+          <div class="card-body ">
+            <p class="card-text "><b>{sg.song}</b></p>
+          </div></div>
+        </div>) : 
+        <div className='text-center mt-3'>
+          {loader?<img  src={loading} alt=""  />: ''}
+        </div>
+        }
+      </div>
     </div>
   )
 }
