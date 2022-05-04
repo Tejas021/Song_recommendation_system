a4e1f56b12e29d5061c4f75171ab32158fa3a2f9
unknown <akshayhegde158@gmail.com>
2022-05-04 15:46:16 +0530

    api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,36 +5,65 @@ import Home from './pages/Home'
 import { useEffect, useState } from "react"
 import Recommender from "./pages/Recommender"
 import { UserContext } from './UserContext'
-import {request} from './axios'
+import { request } from './axios'
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
 
 
 
 function App() {
 
   const [user, setUser] = useState(null)
+  const [songs, setSongs] = useState(null)
   // let navigate = useNavigate();
 
   useEffect(() => {
-    const verifyUser = async ()=>{
-    try {
-      const email = localStorage.getItem('email')
-      const password = localStorage.getItem('password')
-      const resp = await request.post('/login', { 'email': email, "password": password })
-      console.log(resp.data)
-      if (resp.data.user) {
-        localStorage.setItem('email', resp.data.user.email)
-        localStorage.setItem('password', resp.data.user.password)
-        setUser(resp.data)
-        // navigate("/");
-      } else {
-        console.log('error')
+    const verifyUser = async () => {
+      try {
+        const email = localStorage.getItem('email')
+        const password = localStorage.getItem('password')
+        const resp = await request.post('/login', { 'email': email, "password": password })
+        console.log(resp.data)
+        if (resp.data.user) {
+          localStorage.setItem('email', resp.data.user.email)
+          localStorage.setItem('password', resp.data.user.password)
+          setUser(resp.data)
+          // navigate("/");
+        } else {
+          console.log('error')
+        }
+      } catch (err) {
+        console.log(err)
       }
-    } catch (err) {
-      console.log(err)
     }
-  }
-  verifyUser();
+    verifyUser();
+    const getSongs = () => {
+
+
+      const options = {
+        method: 'GET',
+        url: 'https://spotify23.p.rapidapi.com/search/',
+        params: {
+          q: '<REQUIRED>',
+          type: 'multi',
+          // offset: '0',
+          limit: '100',
+          numberOfTopResults: '50'
+        },
+        headers: {
+          'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+          'X-RapidAPI-Key': '00534f03e2msh699ff151af7e151p19cf61jsn09e94e390681'
+        }
+      };
+
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        setSongs(response.data)
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+    getSongs()
   }, [])
 
   return (
@@ -42,9 +71,9 @@ function App() {
       <UserContext.Provider value={{ user, setUser }}>
         <Router>
           <Routes>
-            <Route path="/register" element={user?<Home />:<Signup />} />
-            <Route path="/login" element={user?<Home />:<Login />} />
-            <Route path="/" element={user?<Home />:<Login />} />
+            <Route path="/register" element={user ? <Home /> : <Signup />} />
+            <Route path="/login" element={user ? <Home /> : <Login />} />
+            <Route path="/" element={user ? <Home /> : <Login />} />
             {/* <Route path="/songer" element={<Recommender />} />  */}
           </Routes>
         </Router>
